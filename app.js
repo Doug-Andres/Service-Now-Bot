a0aef83b45eccb3f5a10ea3b7ad5cc4fe2c7cd4d
Doug Andres <30879501+Doug-Andres@users.noreply.github.com>
2018-08-08 07:36:42 -0700

    Conversation Join
    Added code to greet the user that joined the conversation?

--- app.js
+++ app.js
@@ -334,6 +334,31 @@ var bot = new builder.UniversalBot(connector, function (session) {
 })
 .set('storage', cosmosStorage);
 
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        // Say hello
+        var isGroup = message.address.conversation.isGroup;
+        var txt = isGroup ? "Hello everyone!" : "Hello...";
+        var reply = new builder.Message()
+                .address(message.address)
+                .text(txt);
+        bot.send(reply);
+    } else if (message.membersRemoved) {
+        // See if bot was removed
+        var botId = message.address.bot.id;
+        for (var i = 0; i < message.membersRemoved.length; i++) {
+            if (message.membersRemoved[i].id === botId) {
+                // Say goodbye
+                var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                bot.send(reply);
+                break;
+            }
+        }
+    }
+});
+
 // ServiceNow
 bot.dialog('help', function (session) {
     var msg = new builder.Message(session);
